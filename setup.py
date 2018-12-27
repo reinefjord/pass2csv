17eb8ea51103e8ffb79490a3777b1a80a11d8979
Fernando Collova <fernandocollova@gmail.com>
2018-12-27 14:12:06 -0300

    New features and code refactor

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(
+    name='pass2csv',
+    version="1.0.0",
+    description='pass2csv',
+    scripts=['pass2csv.py'],
+    install_requires=["python-gnupg"]
+)
