a349c702cba9be99854b6fd0c6d23cdd143a40d8
Rupus Reinefjord <rupus@reinefjord.net>
2019-01-01 14:58:00 +0100

    start at version 0.1.0

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='pass2csv',
-    version="1.0.0",
+    version="0.1.0",
     description='pass2csv',
     scripts=['pass2csv.py'],
     install_requires=["python-gnupg"]
